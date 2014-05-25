124e5a0ec8ce9c18d5af6d23f9ac6fc04bb1107e
kappa <kappa@kappabookair.local>
2014-05-25 13:13:50 +0900

    delete files.

--- templates/default/plugins/get_cpu_usage.sh
+++
@@ -1,5 +0,0 @@
-function get_cpu_usage() {
-  v=`top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}'`
-  t=90
-  u="Percent"
-}
--- templates/default/plugins/get_memory_used.sh
+++
@@ -1,7 +0,0 @@
-function get_memory_used() {
-  total=`free -m | grep 'Mem' | tr -s ' ' | cut -d ' ' -f 2`
-  free=`free -m | grep 'buffers/cache' | tr -s ' ' | cut -d ' ' -f 4`
-  let "v=100-free*100/total"
-  t=90
-  u="Percent"
-}
